ab6c5628a6357415882163c76b4df1498bebad2f
Inacio19 <inacio.servi@gmail.com>
2025-11-21 10:31:38 -0300

    save

--- meu-portifolio/src/components/projects/ProjectsComponent.jsx
+++ meu-portifolio/src/components/projects/ProjectsComponent.jsx
@@ -4,6 +4,8 @@ import { ProjectsContainer } from "./styleProjectsComponent";
 import BackgroundProjectImage from "../../image/background-image.jpeg";
 import InstagramProjectImage from "../../image/instagram-projetct-portifolio.png";
 import ProjectComponent from "./project/ProjectComponent";
+import { MdOutlineVisibility } from "react-icons/md";
+
 
 const ProjectsComponent = ({ showMenu, showClickMenu }) => {
   return (
