f4becb31112d4b5d27d09a221c320c2b3c598946
Inacio19 <inacio.servi@gmail.com>
2025-11-23 17:22:42 -0300

    atualize node

---
+++ craco.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  webpack: {
+    configure: (webpackConfig) => {
+      webpackConfig.resolve.fallback = {
+        ...webpackConfig.resolve.fallback,
+        path: require.resolve("path-browserify"),
+        os: require.resolve("os-browserify/browser"),
+        crypto: require.resolve("crypto-browserify")
+      };
+      return webpackConfig;
+    },
+  },
+};
